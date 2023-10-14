b1c703a90e5aeadda9da39a3ba5ca92cd11e5834
AI Prince <md.prince0724@gmail.com>
2023-10-15 02:51:30 +0600

    some uodate

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 
-document.addEventListener("mousemove", function (dets) {
+
+//mouse moving effect
+var onMain = document.querySelector(".navMain");
+
+var mouseMove = document.querySelector(".movingCursor");
+onMain.addEventListener("mousemove", function (det) {
+    mouseMove.style.left = det.x + "px";
+    mouseMove.style.top = det.y + "px";
+
+});
+
+// gallery effect
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("mousemove", function (dets) {
     document.querySelectorAll(".img").forEach(element => {
         var position = element.getAttribute("value");
         var X = (window.innerWidth - dets.clientX * position) / 80;
@@ -7,3 +20,22 @@ document.addEventListener("mousemove", function (dets) {
         element.style.transform = `translate(${X}px) translateY(${Y}px)`;
     });
 });
+
+//gsap
+let tl = gsap.timeline();
+tl
+    .from(".leftSide", {
+        opacity: 0,
+        duration: 1,
+        left: -434
+
+    })
+    .from(".heroSec h3", {
+        onStart: function () {
+            $('.heroSec h3').textillate({
+                in: { effect: 'fadeInUp' }
+
+            });
+        }
+    })
+
